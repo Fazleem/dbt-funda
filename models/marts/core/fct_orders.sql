60a1f17d8facae30d4a3ebe243d9663fb760ba1c
fazleem <fazleem.baig@gmail.com>
2022-08-14 17:59:22 -0300

    remove trail spaces

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -16,13 +16,11 @@ order_payments as (
 ),
 
 final as (
-
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
         coalesce(order_payments.amount, 0) as amount
-
     from orders
     left join order_payments using (order_id)
 )
